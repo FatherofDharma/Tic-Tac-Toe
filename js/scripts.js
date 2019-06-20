125a2bf7974333bb1749ac482476c6e3840ec6ce
Daniel Hongel and Sam Cook <daniel@>
2019-06-20 11:26:46 -0700

    clean up spaces and indentations using linter.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-var winCond = [['a','b','c'], ['d','e','f'], ['g','h','i'], ['a','d','g'], ['b','e','h'], ['c','f','i'], ['a','e','i'], ['g','e','c']];
+var winCond = [['a', 'b', 'c'], ['d', 'e', 'f'], ['g', 'h', 'i'], ['a', 'd', 'g'], ['b', 'e', 'h'], ['c', 'f', 'i'], ['a', 'e', 'i'], ['g', 'e', 'c']];
 
-var player = ['f','c','g','a','i'];
+var player = ['f', 'c', 'g', 'a', 'd'];
 
 function win() {
 
@@ -9,15 +9,18 @@ function win() {
     for (var n = 0; n < player.length; n++) {
       var thing = winCond[i].includes(player[n]);
       if (thing) {
-        console.log(winCond[i] + " player has a " + player[n]);
+        console.log(winCond[i] + ' player has a ' + player[n]);
         shouldBeAWin += 1;
       }
+
       console.log(thing);
-      }
-      if (shouldBeAWin === 3) {
-        console.log("player wins");
-        break;
     }
-    console.log("go through the loop again.");
+
+    if (shouldBeAWin === 3) {
+      console.log('player wins');
+      break;
+    }
+
+    console.log('go through the loop again.');
   }
 }
